54bb57cc5b974c81addd54de20eecfeafb75ca6c
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-04-15 03:10:33 +0100

    first commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import DefiVideo from '../assets/video.mp4'
+import React from "react";
+import DefiVideo from "../assets/video.mp4";
 const Hero = () => {
   return (
-    <div className='w-full h-[90vh] top-[90px]'>
-       <video className='object-cover w-full h-full absolute -z-10' src={DefiVideo} autoPlay loop muted /> 
-       <div className='w-full h-[90%] flex flex-col justify-center items-center text-white text-center'>
-           <h1>Decentralized</h1>
-           <h1 className='py-2'> <span className='text-[#00d8ff]'>Trading</span> Protocol</h1>
-           <p className='py-4 text-xl'>Guaranted liquidity trading for million of users and top
-               Ethereum applications
-           </p>
-       <div>
-           <button className='m-2'>Use Defi</button>
-           <button className='m-2'>FAQ</button> 
-           </div>
-       </div>
-       <div>
-           <p className='text-white text-center font-bold text-2xl'>Total Volume Secured: $42,104,783,642.42</p>
-       </div>
+    <div className="w-full h-[90vh] top-[90px]">
+      <video
+        className="object-cover w-full h-full absolute -z-10"
+        src={DefiVideo}
+        autoPlay
+        loop
+        muted
+      />
+      <div className="w-full h-[90%] flex flex-col justify-center items-center text-white text-center">
+        <h1>Decentralized</h1>
+        <h1 className="py-2">
+          {" "}
+          <span className="text-[#00d8ff]">Trading</span> Protocol
+        </h1>
+        <p className="py-4 text-xl">
+          Guaranted liquidity trading for million of users and top Ethereum
+          applications
+        </p>
+        <div>
+          <button className="m-2">Use Defi</button>
+          <button className="m-2">FAQ</button>
+        </div>
+      </div>
+      <div>
+        <p className="text-white text-center font-bold text-2xl">
+          Total Volume Secured: $42,104,783,642.42
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,47 +1,53 @@
-import React,{useState} from 'react'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav,setNav]=useState(false)
-
-    const handleClick = () =>{
-          setNav(!nav)
-    }
+  const handleClick = () => {
+    setNav(!nav);
+  };
   return (
-    <div className='w-full h-[90px] bg-black'>
-        <div className='max-w-[1240px] flex mx-auto justify-between px-4 h-full  items-center'>
-                 <div>
-                   <h1 className='text-[#00d8ff] font-bold text-3xl '>DEFI</h1>
-                 </div>
-            <div className='hidden md:flex'>
-                <ul className='flex text-white items-center'>
-                    <li className='px-4'>Platform</li>
-                    <li className='px-4'>Developers</li>
-                    <li className='px-4'>Community</li>
-                    <li className='px-4'>About</li>
-                    <button className='ml-4'>Use Defi</button>
-                </ul>
-            </div>
-            {/*Hamburger-menu*/}
-            <div onClick={handleClick} className='block md:hidden'>
-                {nav? <AiOutlineClose size={30} className='text-white' />: <AiOutlineMenu size={30} className='text-white' />}
-                   
-            </div>
-            {/*Mobile-menu*/}
-            <div className={nav? 'w-full text-white left-0 top-[90px] absolute bg-black flex justify-center text-center'
-            : 'absolute left-[-100%]'}>
-                <ul>
-                    <li className='text-2xl p-4'>Platform</li>
-                    <li className='text-2xl p-4'>Developers</li>
-                    <li className='text-2xl p-4'>Community</li>
-                    <li className='text-2xl p-4'>About</li>
-                    <button className='ml-4'>Use Defi</button>
-                </ul>
-            </div>
+    <div className="w-full h-[90px] bg-black">
+      <div className="max-w-[1240px] flex mx-auto justify-between px-4 h-full  items-center">
+        <div>
+          <h1 className="text-[#00d8ff] font-bold text-3xl ">DEFI</h1>
         </div>
-         
+        <div className="hidden md:flex">
+          <ul className="flex text-white items-center">
+            <li className="px-4">Platform</li>
+            <li className="px-4">Developers</li>
+            <li className="px-4">Community</li>
+            <li className="px-4">About</li>
+            <button className="ml-4">Use Defi</button>
+          </ul>
+        </div>
+        {/*Hamburger-menu*/}
+        <div onClick={handleClick} className="block md:hidden">
+          {nav ? (
+            <AiOutlineClose size={30} className="text-white" />
+          ) : (
+            <AiOutlineMenu size={30} className="text-white" />
+          )}
+        </div>
+        {/*Mobile-menu*/}
+        <div
+          className={
+            nav
+              ? "w-full text-white left-0 top-[90px] absolute bg-black flex justify-center text-center"
+              : "absolute left-[-100%]"
+          }
+        >
+          <ul>
+            <li className="text-2xl p-4">Platform</li>
+            <li className="text-2xl p-4">Developers</li>
+            <li className="text-2xl p-4">Community</li>
+            <li className="text-2xl p-4">About</li>
+            <button className="ml-4">Use Defi</button>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
